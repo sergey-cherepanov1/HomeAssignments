87fb377fc99861a81528f26c26e4fbdd66284f93
kaido <st129987@student.spbu.ru>
2024-09-13 21:53:33 +0300

    Added main.cpp

---
+++ task-1/main.cpp
@@ -0,0 +1,11 @@
+/* Sergey Cherepanov st129987@student.spbu.ru
+   task-1 project
+*/
+
+#include <iostream>
+
+int main(int arc, char *argv[])
+{
+	std::cout << "Hello World!" << std::endl;
+	Hello()
+}
