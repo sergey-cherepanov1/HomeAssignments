8e5e42adf15a52611614642d5007e1e835e6fc4f
kaido <st129987@student.spbu.ru>
2024-09-13 22:19:14 +0300

    Added hello.cpp that contains function which used in main.cpp

---
+++ task-1/hello.cpp
@@ -0,0 +1,13 @@
+/* Sergey Cherepanov st129987@student.spbu.ru
+  task-1 project
+*/
+
+#include <iostream>
+#include <string>
+
+void Hello()
+{
+	std::string s;
+	getline(std::cin, s);
+	std::cout << "Hello" << s << std::endl; 
+}
