4fcda9ab5909f9b4d573ca20965afe19f1b22f21
kaido <st129987@student.spbu.ru>
2024-10-04 18:08:48 +0300

    Wrapped the contents of the main function in a while loop and added console output that said "Enter an entry in reverse Polish notation" before reading the line, and also added "Answer: " before outputting the response.

--- Assignment2b/main.cpp
+++ Assignment2b/main.cpp
@@ -6,45 +6,49 @@
 
 int main()
 {
-	std::string str;
-	std::getline(std::cin, str);
-	int *start = new int[str.length()];
-	for (int i = 0; i < str.length(); i++)
+	while (true)
 	{
-                switch (str[i])
-                {
-                        case '+':
-                                *(start - 2) = *(start - 2) + *(start - 1);
-                                start--;
-                                i++;
-                                break;
-			case '-':
-                                *(start - 2) = *(start - 2) - *(start - 1);
-                                start--;
-                                i++;
-                                break;
-			case '*':
-                                *(start - 2) = *(start - 2) * *(start - 1);
-                                start--;
-                                i++;
-                                break;
-			case '/':
-                                *(start - 2) = *(start - 2) / *(start - 1);
-                                start--;
-                                i++;
-                                break;
-                        default:
-                                std::string temp;
-                                while (str[i] != ' ')
-                                {
-                                        temp += str[i];
-                                        i++;
-                                }
-                                *start = stoi(temp);
-                                start++;
-                }
-        }
-	start--;
-	std::cout << *start << std::endl;
-	delete[] start;
+		std::string str;
+		std::cout << "Введите строку в формате ОПЗ:" << std::endl;
+		std::getline(std::cin, str);
+		int *start = new int[str.length()];
+		for (int i = 0; i < str.length(); i++)
+		{
+		        switch (str[i])
+		        {
+		                case '+':
+		                        *(start - 2) = *(start - 2) + *(start - 1);
+		                        start--;
+		                        i++;
+		                        break;
+				case '-':
+		                        *(start - 2) = *(start - 2) - *(start - 1);
+		                        start--;
+		                        i++;
+		                        break;
+				case '*':
+		                        *(start - 2) = *(start - 2) * *(start - 1);
+		                        start--;
+		                        i++;
+		                        break;
+				case '/':
+		                        *(start - 2) = *(start - 2) / *(start - 1);
+		                        start--;
+		                        i++;
+		                        break;
+		                default:
+		                        std::string temp;
+		                        while (str[i] != ' ')
+		                        {
+		                                temp += str[i];
+		                                i++;
+		                        }
+		                        *start = stoi(temp);
+		                        start++;
+		        }
+		}
+		start--;
+		std::cout << "Ответ: " << *start << std::endl;
+		delete[] start;
+	}
 }
