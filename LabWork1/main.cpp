450e7d9218a3855b2bd823f523ca7828d5aeff41
kaido <st129987@student.spbu.ru>
2024-11-06 21:03:16 +0300

    Created bmp loading function and made a header for bmp structs

--- LabWork1/main.cpp
+++ LabWork1/main.cpp
@@ -1,29 +1,45 @@
+#include "bmp.h"
 #include <iostream>
 #include <fstream>
 
-#pragma pack(push, 1) // used to remove struct alignment, make it equals to 1 byte
-struct BMPFileHeader {
-    uint16_t file_type = 0x4D42; // File type BM is 0x4D42 
-    uint32_t file_size = 0;      // Total file size
-    uint16_t reserved1 = 0;      // reserved service field 
-    uint16_t reserved2 = 0;      // reserved service field
-    uint32_t offset_data = 54; // The offset in the file at which the image data begins. Usually 54 bytes for 24-bit BMP
-};
+bool loadBMP(const std::string& filename, std::vector<uint8_t>& imageData, int& width, int& height) {
+    std::ifstream file(filename, std::ios::binary);
+    if (!file.is_open()) {
+        std::cout << "Error opening file!" << std::endl;
+        return false;
+    }
 
-struct BMPInfoHeader {
-     uint32_t header_size = 40; // Header size, 40 bytes usually
-     int32_t width = 0; // Image width
-     int32_t height = 0; // Image height
-     uint16_t colour_planes = 1; // Amount of colour planes, always 1
-     uint16_t bits_per_pixel = 24; // Color depth in bits per pixel (24 bits for color, 8 for gray)
-     uint32_t compression = 0; // compression method (0 - no compression)
-     uint32_t image_image = 0; // image size in bytes
-     int32_t x_resolution = 0; // number of pixels per meter along the X axis (not used)
-     int32_t y_resolution = 0; // number of pixels per meter along the Y axis (not used)
-     uint32_t colours = 0; // Number of colors in the palette (0 if bits_per_pixel > 8)
-     uint32_t colors_important = 0; // Number of important colors in the palette
-};
+    BMPFileHeader fileHeader;
+    file.read((char*)&fileHeader, sizeof(fileHeader));
 
-#pragma pack(pop)
+    if (fileHeader.file_type != 0x4D42) {
+        std::cout << "Not a BMP file!" << std::endl;
+        return false;
+    }
 
+    BMPInfoHeader InfoHeader;
+    file.read((char*)&InfoHeader, sizeof(InfoHeader));
+
+    width = InfoHeader.width;
+    height = InfoHeader.height;
+
+    // Calculating the image row size with alignment to a 4 bytes.
+    // Row size in bytes
+    int rowSize = (width * InfoHeader.bits_per_pixel + 31) / 32 * 4;
+
+    // Size of all image data
+    int imageSize = rowSize * height;
+
+    // Allocating memory for image data
+    imageData.resize(imageSize);
+
+    // Move the read pointer to where the image data begins
+    file.seekg(fileHeader.dataOffset, std::ios::beg);
+
+    // Reading image data
+    file.read((char*)imageData.data(), imageSize);
+
+    file.close();
+    return true;
+}
 
